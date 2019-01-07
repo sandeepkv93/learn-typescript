1730da66c6c4b8ecff130a6d4be7cec56066763a
Sandeep Vishnu <sandeepkv93@gmail.com>
2019-01-07 10:41:33 +0530

    Implicit Types in typescript

---
+++ implicit-types.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = true;
+var c = "hello";
+// a = true; Doesn't work, gives the error: Type 'true' is not assignable to type 'number'
+// Based on initial assignment, typescript assigns an implicit type.
+function greet() {
+    return 'Good Morning';
+}
+var greeting = greet();
+// greeting = 10; Doesn't work
+// But if you separate decalaration and initalization in a separate line, implicit typing would be 'any'
+var newGreeting;
+newGreeting = greet();
+console.log(typeof newGreeting); // Gives any
